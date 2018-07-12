b38ee8cebb6f3236f5dd2a0838f03a22eaf57db8
Eder Trindade <eder.gabriel.felix@gmail.com>
2018-07-12 19:33:24 -0300

    Added i3 and i3blocks config

--- setup.sh
+++ setup.sh
@@ -6,20 +6,26 @@ if ! [ -x "$(command -v zsh)" ]; then
 fi
 
 # Set zsh as default shell
-chsh -s $(which zsh)
+if [[ "$SHELL" != *zsh ]]; then
+  chsh -s $(which zsh)
+fi
 
 # Install ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
 
 #Install Spacemacs
-if [ ! -d "~/.emacs.d" ]; then
+if [ ! -d "$HOME/.emacs.d" ]; then
   git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 fi
 
-function safe_copy {
-  [ -f $HOME/$1 ] && mv $HOME/$1 "$HOME/$1.bk$(date +%Y%m%d%H%M%S)"
-  ln -s $HOME/dot-files/$1 $HOME/$1
+function safe_link {
+  [ -f $HOME/$1 ] && echo $1 already exists && return #mv $HOME/$1 "$HOME/$1.bk$(date +%Y%m%d%H%M%S)"
+  ln -s -T $HOME/dot-files/$1 $HOME/$1 && echo $1 symlinked
 }
 
-safe_copy .spacemacs
-safe_copy .zshrc
+safe_link .spacemacs
+safe_link .zshrc
+safe_link .config/i3
+safe_link .config/i3blocks
