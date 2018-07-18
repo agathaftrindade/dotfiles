cc7a87bb7e3483b31bd662a4086bfc683ffc5546
Eder Trindade <eder.gabriel.felix@gmail.com>
2018-07-18 00:10:44 -0300

    Audio shortcuts, i3conf alias

--- z/aliases.zsh
+++ z/aliases.zsh
@@ -16,6 +16,8 @@ alias dotf="~/dotfiles"
 alias ealias="vi ~/dotfiles/z/aliases.zsh && rel"
 alias rel="source ~/.zshrc"
 
+alias i3conf="vim ~/.config/i3/config"
+
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
